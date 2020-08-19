78d742a7dc286180c4f6dcfeb18e819f25bd4798
ida4594 <kaha4594@gmail.com>
2020-08-19 20:17:47 +0900

    CSV取込

--- app/models/sale.rb
+++ app/models/sale.rb
@@ -1,10 +1,10 @@
 class Sale < ApplicationRecord
   belongs_to :shop, foreign_key: 'shop_id'
 
-  def self.import(file)
+def self.import(file)
     CSV.foreach(file.path, headers: true) do |row|
       # IDが見つかれば、レコードを呼び出し、見つかれなければ、新しく作成
-      sale = find_by(shop_id: row["shop_id"]) || new
+      sale = find_by(id: row["id"]) || new
       # CSVからデータを取得し、設定する
       sale.attributes = row.to_hash.slice(*updatable_attributes)
       # 保存する
@@ -14,6 +14,6 @@ class Sale < ApplicationRecord
 
   # 更新を許可するカラムを定義
   def self.updatable_attributes
-    ["shop_id","rate"]
+    ["id", "shop_id", "rate"]
   end
 end
